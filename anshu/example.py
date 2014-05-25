aa6e2726c3e20f8313cb1ef72bc3bb365df98ad2
Anshu Aviral <aviral.2815@gmail.com>
2014-05-25 14:16:04 +0530

    update example.py
    changed working of join to join datai correctly without using lst argument

--- anshu/example.py
+++ anshu/example.py
@@ -10,37 +10,47 @@ dataf=
   2.07 -0.4817... 0.87630  //  angle= 0.33*2*pi
   0.03                     //  kv=0.03
 """
-
+import math
 
 class Sequence(object):
-   def __init__(self,coord):
-     self.datai=[]
-     self.dataf=[]
-     self.coord=coord
-   def add_rot(self,angle):
-     newid=len(self.datai) 
-     newfid=len(self.dataf)
-     self.datai.extend([newid,1,newfid,None])
-     self.dataf.append([angle,cos(angle),sin(angle)])
-     return newid
-   def add_kick(self,kv,ord):
-     newid=len(self.datai)
-     newfid=len(self.dataf)
-     self.datai.extend([newid,2,newfid,None,ord])
-     self.dataf.append([kv])
-     return newid
-   def join(self,lst):
-     for e1,e2 in zip(lst[:-1],lst[1:]):
-       self.datai[e1+3]=e2
-     self.datai[e2+3]=lst[0]
-     return self
-   def track(self,lst):
-     libc.vec_map_loop(array(self.datai),array(self.dataf),self.coord)
-     return seq
+	def __init__(self,coord):
+		self.datai=[]
+		self.dataf=[]
+		self.coord=coord
+	def add_rot(self,angle):
+		newid=len(self.datai)
+		newfid=len(self.dataf)
+		self.datai.extend([newid,1,newfid,None])
+		self.datai.append([])
+		self.dataf.append([angle,math.cos(angle),math.sin(angle)])
+		return newid
+	def add_kick(self,kv,order):
+		newid=len(self.datai)
+		newfid=len(self.dataf)
+		self.datai.extend([newid,2,newfid,None])
+		self.datai.append([order])
+		self.dataf.append([kv])
+		return newid
+	def add_counter(self,limit,count):
+		newid=len(self.datai)
+		newfid=len(self.dataf)
+		self.datai.extend([newid,3,newfid,None])
+		self.datai.append([limit,count])
+		self.dataf.append([])
+	def join(self):
+		length = len(self.datai)
+		e1=0
+		while e1<=(length-10): 
+			self.datai[e1+3]=self.datai[e1+5]
+			e1=e1+5
+		self.datai[e1+3]=self.datai[0]
+		return self
+	def track(self):
+		libc.vec_map_loop(array(self.datai),array(self.dataf),self.coord)
+		return seq
 
-seq=Sequence()
-rot=seq.add_rot(0.33*2*pi)
+seq=Sequence([0,0])
+rot=seq.add_rot(0.33*2*math.pi)
 kick=seq.add_kick(0.02,2)
-count=seq.add_counter(100)
+count=seq.add_counter(100,2)
 seq=seq.join().track()
-
