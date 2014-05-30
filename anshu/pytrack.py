9bdacfca21ada4ac81b61814d532b2b1ac691a57
Anshu Aviral <aviral.2815@gmail.com>
2014-05-30 13:47:44 +0530

    added parameter coordi[] to sequence

--- anshu/pytrack.py
+++ anshu/pytrack.py
@@ -17,68 +17,71 @@ argi = ThreeInt(0)		#format of argi - [ord]
 coord = TwoFloat(0,1)		#format of coord - [x,y]
 
 class Sequence(object):
-	def __init__(self,coord):
-		self.datai=[]
-		self.dataf=[]
-		self.coord=coord
-		self.lst=[]
-	def add_rot(self,angle):
-		newid=len(self.datai)
-		newfid=len(self.dataf)
-		self.lst.extend([newid])
-		self.datai.extend([newid,1,newfid,None])
-		self.dataf.extend([angle,math.cos(angle),math.sin(angle)])
-		return newid
-	def add_kick(self,kv,order):
-		newid=len(self.datai)
-		newfid=len(self.dataf)
-		self.lst.extend([newid])
-		self.datai.extend([newid,3,newfid,None,order])
-		self.dataf.extend([kv])
-		return newid
-	def add_counter(self,limit,count):
-		newid=len(self.datai)
-		newfid=len(self.dataf)
-		self.lst.extend([newid])
-		self.datai.extend([newid,3,newfid,None,limit,count])
-		self.dataf.extend([])
-		return newid
-	def join(self):
-		self.lst.extend([None])		
-		i=1
-		for index, value in enumerate(self.datai): 
-			if value==None:
-				if self.lst[i]==None:
-					self.datai[index]=self.datai[0]
-				else:
-					self.datai[index]=self.lst[i]
-					i = i+1
-		return self
-	def track(self,np,nturn,seq):
-		self.join()
-		c_np = ctypes.c_int(np)
-		c_nturn = ctypes.c_int(nturn)
-		c_seq = ctypes.c_int(seq)
-		arg_datai=(ctypes.c_int *len(self.datai))()
-		arg_dataf=(ctypes.c_double *len(self.dataf))()
-		arg_coord=(ctypes.c_double * len(self.coord))()
-		for index, value in enumerate(self.datai):	
-			arg_datai[index] = int(value) 
-		for index, value in enumerate(self.dataf):
-			arg_dataf[index] = float(value)
-		for index, value in enumerate(self.coord):
-			arg_coord[index] = float(value)
-		val = libc.vec_map_loop(arg_dataf,arg_datai,arg_coord,c_np,c_nturn,c_seq)
-		print val		#for testing 
-		return self
+  def __init__(self,coord,coordi):
+    self.datai=[]
+    self.dataf=[]
+    self.coord=coord
+    self.coordi=coordi
+    self.lst=[]  
+  def add_rot(self,angle):
+    newid=len(self.datai)
+    newfid=len(self.dataf)
+    self.lst.extend([newid])
+    self.datai.extend([newid,1,newfid,None])
+    self.dataf.extend([angle,math.cos(angle),math.sin(angle)])
+    return newid
+  def add_kick(self,kv,order):
+    newid=len(self.datai)
+    newfid=len(self.dataf)
+    self.lst.extend([newid])
+    self.datai.extend([newid,2,newfid,None,order])
+    self.dataf.extend([kv])
+    return newid
+  def add_counter(self,limit,count):
+    newid=len(self.datai)
+    newfid=len(self.dataf)
+    self.lst.extend([newid])
+    self.datai.extend([newid,3,newfid,None,limit,count])
+    self.dataf.extend([])
+    return newid
+  def join(self):
+    self.lst.extend([None])		
+    i=1
+    for index, value in enumerate(self.datai): 
+      if value==None:
+        if self.lst[i]==None:
+          self.datai[index]=self.datai[0]
+        else:
+          self.datai[index]=self.lst[i]
+          i = i+1
+    return self
+  def track(self,nturn,seq):
+    self.join()
+    c_nturn = ctypes.c_int(nturn)
+    c_seq = ctypes.c_int(seq)
+    arg_coordi=(ctypes.c_int *len(self.coordi))()
+    arg_datai=(ctypes.c_int *len(self.datai))()
+    arg_dataf=(ctypes.c_double *len(self.dataf))()
+    arg_coord=(ctypes.c_double * len(self.coord))()
+    for index, value in enumerate(self.datai):	
+      arg_datai[index] = int(value) 
+    for index, value in enumerate(self.dataf):
+      arg_dataf[index] = float(value)
+    for index, value in enumerate(self.coord):
+      arg_coord[index] = float(value)
+    for index, value in enumerate(self.coordi):
+      arg_coordi[index] = int(value)
+    val = libc.vec_map_loop(arg_dataf,arg_datai,arg_coord,arg_coordi)
+    print val	  #for testing 
+    return self
 
 #adding features
-seq=Sequence([0,0])
+seq=Sequence([0,0],[1,2])
 rot=seq.add_rot(0.33*2*math.pi)
 kick=seq.add_kick(0.02,2)
 kick1=seq.add_kick(0.01,3)
-count=seq.add_counter(1,0)
-seq=seq.track(1,2,3)
+count=seq.add_counter(10,0)
+seq=seq.track(1,2)
 
 #testing
 var = libc.update_rot2d(argf,argi)
