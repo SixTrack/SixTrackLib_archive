2bb2a2124b22655e7aacecf1739735d5583555f3
kartikeya singh <kartikeya107@gmail.com>
2014-06-04 03:48:47 +0530

    maps for quadrupole, sextupole, decapole, dodecapole, 14,16,18,20 pole and octupole

--- kartikeya/sixtrack_lib/SixTrack/SixTrack_4518_cernlib_crlibm_gfortran_O4/test.c
+++ kartikeya/sixtrack_lib/SixTrack/SixTrack_4518_cernlib_crlibm_gfortran_O4/test.c
@@ -3,8 +3,51 @@
 #define coord(i) coord[i-1]
 #define argf(i)  argf[i-1]
 #define argi(i)  argi[i-1]
+#define npart 0
+#define napx 1
+#define strackc 0
+#define stracks 1
+#define xsiv 2
+#define zsiv 3
+#define tiltc 4
+#define tilts 5
+#define crkve 6
+#define cikve 7
+#define xv(i,j) (i-1)*argi[npart]+j-1
+#define yv(i,j) (2+i-1)*argi[npart]+j-1
+#define xlv(j) 4*argi[npart]+j-1
+#define zlv(j) 5*argi[npart]+j-1
+#define oidpsv(j) 6*argi[npart]+j-1
 
-extern int map_hr_2_(double *coord,double *argf,int *argi)
+extern int thin6d_map_quadrupole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+extern int thin4d_map_quadrupole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+extern int thin6d_map_horizontal_dipole_(double *coord,double *argf,int *argi)
 {
 int j;
 for(j=1;j<=argi[1];j++)
@@ -15,91 +58,242 @@ coord[3*argi[0]+j-1]+=argf[1]*coord[6*argi[0]+j-1];
 return 1;
 }
 
-extern int map_thin6d_quadrupole_(double *coord,double *argf,int *argi)
+extern int thin4d_map_horizontal_dipole_(double *coord,double *argf,int *argi)
 {
 int j;
 for(j=1;j<=argi[1];j++)
 {
-coord(4*argi[0]+j)=(coord(j)-argf(3))*argf(5)+(coord(argi[0]+j)-argf(4))*argf(6);
-coord(5*argi[0]+j)=(coord(argi[0]+j)-argf(4))*argf(5)-(coord(j)-argf(3))*argf(6) ;                                
-argf(7)=coord(4*argi[0]+j);
-argf(8)=coord(5*argi[0]+j);
-coord[2*argi[0]+j-1]+=coord[6*argi[0]+j-1]*(argf[0]*argf(7)+argf[1]*argf(8));
-coord[3*argi[0]+j-1]+=coord[6*argi[0]+j-1]*(argf[1]*argf(7)-argf[0]*argf(8)) ;  
-}                                               
+coord[2*argi[0]+j-1]+=argf[0]*coord[6*argi[0]+j-1];
+coord[3*argi[0]+j-1]+=argf[1]*coord[6*argi[0]+j-1];
+}
 return 1;
 }
 
-extern int mp(double *v1,double *v2,double *v3,double *v4,double *v5,double *v6,double *v7)
-{
-*v1 = (*v2-(*v3))*(*v4)+(*v5-(*v6))*(*v7);
-return 1;
-}
-
-extern int mp1(double *var1,double *var2,double *var3,double *argf)
-{
-*var1=(*var2-argf[9])*argf[11]+(*var3-argf[10])*argf[12];
-return 1;
-}
-/*
-//<<<<<<< HEAD
-
-extern int thin6d_track1_(double *yv,double *var1,double *var2,int *n)
-{
-int i;
-FILE *fp = fopen("tempo1","w");
-fprintf(fp,"value of yv - %40.20f\n value of var1 - %40.20f\n value of var2 - %40.20f\n value of napx - %d\n",*yv,*var1,*var2,*n);
-
-//for(i=1;i<=(*n);i++)
-//*yv += (*var1)*(*var2);
-fprintf(fp,"value of yv - %40.20f\n",*yv);
-fclose(fp);
-return 1;
-}
-extern int thin6d_track2_(double *yv,double *var1,double *var2,int *n)
-{
-int i;
-FILE *fp = fopen("tempo2","w");
-fprintf(fp,"value of yv - %40.20f\n value of var1 - %40.20f\n value of var2 - %40.20f\n value of napx - %d\n",*yv,*var1,*var2,*n);
-
-//for(i=1;i<=(*n);i++)
-//*yv += (*var1)*(*var2);
-fprintf(fp,"value of yv - %40.20f\n",*yv);
-fclose(fp);
-return 1;
-}
-
-//extern int thin6d_track_(double *var1,double *var2,double *var3)
-//=======
-/*extern int thin6d_track_hor_dipole_(double* argf,double *argi,double*coord)
-//>>>>>>> 31b687648ee5a8217c5bd2d3f33284b34de5a82b
-{
-*var1 += (*var2)*(*var3);
-return 1;
-}
-
-extern int  file_write_(double yv)
-{
-FILE *fp = fopen("tempo1","w");
-fprintf(fp,"%40.20f",yv);
-fclose(fp);
-}
-*/
-/*
-extern int thin6d_singarray_(double *coord,double *argf,double *argi,int *j)
-{
-coord[0+(*j)*(*j)]+=argf[0]*argi[(*j)*(*j)];
-coord[1+(*j)*(*j)]+=argf[1]*argi[(*j)*(*j)];
-return 1;
-}
-extern int map_thin6d_hor_dipole_(double *coord,double *argf,int *argi)
+extern thin6d_map_sextupole_(double *coord,double *argf,int *argi)
 {
 int j;
 for(j=1;j<=argi[1];j++)
 {
-coord[j*j]+=argf[0]*argf[2+j-1];
-coord[j*j+1]+=argf[1]*argi[2+j-1];
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
 }
 return 1;
-}*/
+}
 
+extern thin6d_map_norm_octupole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+
+extern thin6d_map_norm_decapole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+
+extern thin6d_map_norm_dodecapole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+
+extern thin6d_map_norm_14pole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+
+extern thin6d_map_norm_16pole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+
+extern thin6d_map_norm_18pole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
+
+
+extern thin6d_map_norm_20pole_(double *coord,double *argf,int *argi)
+{
+int j;
+for(j=1;j<=argi[1];j++)
+{
+coord[4*argi[npart]+(j)-1]=(coord[j-1]-argf[2])*argf[4]+(coord[argi[0]+(j)-1]-argf[3])*argf[5];
+coord[5*argi[npart]+(j)-1]=(coord[j+argi[0]-1]-argf[3])*argf[4]-(coord[(j)-1]-argf[2])*argf[5];
+argf[6]=coord[4*argi[npart]+(j)-1];
+argf[7]=coord[5*argi[npart]+(j)-1];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+argf[8]=argf[6]*coord[4*argi[npart]+j-1]-argf[7]*coord[5*argi[npart]+j-1];
+argf[7]=argf[6]*coord[5*argi[npart]+j-1]+argf[7]*coord[4*argi[npart]+j-1];
+argf[6]=argf[8];
+coord[2*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[0]*argf[6]+argf[1]*argf[7]);
+coord[3*argi[npart]+(j)-1]+=coord[6*argi[npart]+(j)-1]*(argf[1]*argf[6]-argf[0]*argf[7]);
+}
+return 1;
+}
