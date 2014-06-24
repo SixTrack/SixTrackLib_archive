66dc67879e74dd3d42fc2a6e2de03a9e5789ebf2
Riccardo De Maria <riccardodemaria@gmail.com>
2014-06-24 13:28:16 +0200

    adding new version from anshuw with RPN evaluator

---
+++ anshu/rpncalc/test1.py
@@ -0,0 +1,252 @@
+import ctypes, math
+from ctypes import *
+
+ctypes.cdll.LoadLibrary("/home/aviral/Desktop/CERN code/Start/track.so") #or path of the shared object
+libc = ctypes.CDLL("/home/aviral/Desktop/CERN code/Start/track.so") 	#or path of the shared object
+
+class RPNparser(object):
+  parser = []
+  constf = []
+
+  op_add = "+"
+  op_sub = "-"
+  op_equ = "="
+  op_mul = "*"
+  op_div = "/"
+  op_left = "("
+  op_right = ")"
+  op_pow = "^"
+
+  encode_opr={'+':3,'-':6,'=':9,'*':12,'/':15,'^':18}
+
+  op_associativity = {op_pow: "right", op_mul: "left", op_div: "left", op_add: "left", op_sub: "left", op_equ: "left"}
+
+  op_precedence = {op_pow: 4, op_mul: 3, op_div: 3, op_add: 2, op_sub: 2, op_equ: 1, op_left: -1, op_right: -1}
+
+  def precedence(self,op):
+    return self.op_precedence[op]
+
+  def associativity(self,op):
+    return self.op_associativity[op]
+
+  def is_operator(self,c):
+    return self.op_precedence.has_key(c) and not (self.is_left(c) or self.is_right(c))
+
+  def is_left(self,c):
+    return c == self.op_left
+
+  def is_right(self,c):
+    return c == self.op_right
+
+  def append_token(self,token, l):
+    if token != "":
+        l.append(token)
+    return ""
+
+  def gen_token(self,expr):
+    tokens = []
+    last_token = ""
+    in_token = False
+    for c in expr:
+        if c == " ":
+            in_token = False
+            last_token = self.append_token(last_token, tokens)
+            continue
+        elif self.is_operator(c) or self.is_right(c) or self.is_left(c):
+            in_token = False
+            last_token = self.append_token(last_token, tokens)
+            tokens.append(c)
+        else:
+            in_token = True
+            last_token += c
+        if not in_token and last_token != "":
+            last_token = self.append_token(last_token, tokens)
+    if in_token:
+        self.append_token(last_token, tokens)
+    return tokens
+
+  def __init__(self,seq,expr):
+    ops = []
+    results = []
+    for token in self.gen_token(expr):
+        #print ops, results
+        if self.is_operator(token):
+            while len(ops) > 0 and self.is_operator(ops[-1]) and \
+                    ( (self.associativity(token) == 'left' and self.precedence(token) <= self.precedence(ops[-1])) \
+                 or   (self.associativity(token) == 'right' and self.precedence(token) < self.precedence(ops[-1])) ):
+                results.append(ops.pop())
+            ops.append(token)
+        elif self.is_left(token):
+            ops.append(token)
+        elif self.is_right(token):
+            while len(ops) > 0 and not self.is_left(ops[-1]):
+                results.append(ops.pop())
+            if len(ops) == 0:
+                print "error: mismatched parentheses"
+                exit()
+            if self.is_left(ops[-1]):
+                ops.pop()
+        else:
+            results.append(token)
+    while len(ops) > 0:
+        if self.is_right(ops[-1]) or self.is_left(ops[-1]):
+            print "error: mismatched parentheses"
+            exit()
+        results.append(ops.pop())
+    self.parser,self.constf = self.encode(seq,results)  
+
+  def encode(self,seq,expr):
+    enc_res=[]
+    constf=[]
+    for tok in expr:
+        if tok.isdigit()==True:
+            enc_res.append(3*eval(tok)+1)
+        elif tok.replace('.','',1).isdigit():
+            enc_res.extend([3*len(constf)+1,21])
+            constf.append(eval(tok))
+        elif self.encode_opr.has_key(tok):
+            enc_res.append(self.encode_opr[tok])
+        else:
+            #print seq.search(tok)
+            enc_res.append(seq.search(tok))
+    seq.datai.extend([len(enc_res)])
+    seq.datai.extend(enc_res)
+    seq.dataf.extend(constf)
+    return enc_res, constf
+
+class Rot2d(object):
+  _typeid=1
+  def __init__(self,Element,**argsv):
+    self._parameters={'angle':(0.0,1,0),'cx':(1.0,1,1),'sx':(0.0,1,2)}
+    self.name = Element
+    keys = sorted(argsv.keys())
+    self._parameters[keys[0]]=(argsv[keys[0]],1,0)
+    self._parameters['cx'] = (math.cos(argsv[keys[0]]),1,1)
+    self._parameters['sx'] = (math.sin(argsv[keys[0]]),1,2)
+
+class Kick2d(object):
+  _typeid=2
+  def __init__(self,Element,**argsv):
+    self._parameters={'kv':(0.0,1,0),'order':(0,0,0)}
+    self.name = Element
+    keys = sorted(argsv.keys())
+    self._parameters[keys[0]]=(argsv[keys[0]],1,0)
+    self._parameters[keys[1]]=(argsv[keys[1]],0,0)
+
+class Counter(object):
+  _typeid=3
+  def __init__(self,Element,**argsv):
+    self._parameters={'limit':(0,0,0),'count':(0,0,1)}
+    self.name = Element
+    keys = sorted(argsv.keys())
+    self._parameters[keys[0]]=(argsv[keys[0]],0,0)
+    self._parameters[keys[1]]=(argsv[keys[1]],0,1)
+
+class Expression(object):
+  _typeid=4
+  def __init__(self,Element,**argsv):
+    self.name = Element
+
+class Sequence(object):
+  datai=[]
+  dataf=[]
+  coord=[]
+  coordi=[]
+
+  def __init__(self):
+    self.elems={}
+    self.lst=[]  
+
+  def add_rot(self,label,angle):
+    newid=len(self.datai)
+    newfid=len(self.dataf)
+    self.lst.extend([newid])
+    self.datai.extend([newid,1,newfid,None])
+    self.dataf.extend([angle,math.cos(angle),math.sin(angle)])
+    self.add_elem(label,Rot2d,newid,angle=angle)
+
+  def add_kick(self,label,kv,order):
+    newid=len(self.datai)
+    newfid=len(self.dataf)
+    self.lst.extend([newid])
+    self.datai.extend([newid,2,newfid,None,order])
+    self.dataf.extend([kv])
+    self.add_elem(label,Kick2d,newid,kv=kv,order=order)
+
+  def add_count(self,label,limit,count):
+    newid=len(self.datai)
+    newfid=len(self.dataf)
+    self.lst.extend([newid])
+    self.datai.extend([newid,3,newfid,None,limit,count])
+    self.dataf.extend([])
+    self.add_elem(label,Counter,newid,limit=limit,count=count)
+
+  def add_expr(self,label,var_into,expr):
+    newid=len(self.datai)
+    newfid=len(self.dataf)
+    self.lst.extend([newid])
+    res_into = self.search(var_into)
+    self.datai.extend([newid,4,newfid,None,res_into])
+    RPNparser(self,expr)
+    self.add_elem(label,Expression,newid)   
+
+  def add_elem(self,name,eltype,elem_id,**argsv):
+    el=eltype(name,**argsv)
+    self.elems[name]=(el,elem_id)
+
+  def search(self,label):
+    tag=label.split('.')  
+    if self.elems[tag[0]][0]._parameters.has_key(tag[1])==True:
+        main = self.elems[tag[0]][1]
+        choice = self.elems[tag[0]][0]._parameters[tag[1]][1]
+        offset = self.elems[tag[0]][0]._parameters[tag[1]][2]
+    if choice==0:
+      ret = 2*(main+offset+4)
+    if choice==1:
+      ret = 2*(self.datai[main+2]+offset)+1
+    return 3*ret+2
+
+  def join(self):
+    self.lst.extend([None])		
+    i=1
+    for index, value in enumerate(self.datai): 
+      if value==None:
+        if self.lst[i]==None:
+          self.datai[index]=self.datai[0]
+        else:
+          self.datai[index]=self.lst[i]
+          i = i+1
+    return self
+
+  def track(self,coord,coordi):
+    self.join()
+    self.coord = coord
+    self.coordi = coordi
+    arg_coordi=(ctypes.c_int *len(self.coordi))()
+    arg_datai=(ctypes.c_int *len(self.datai))()
+    arg_dataf=(ctypes.c_double *len(self.dataf))()
+    arg_coord=(ctypes.c_double * len(self.coord))()
+    arg_coordi=(ctypes.c_int * len(self.coordi))()
+    for index, value in enumerate(self.datai):	
+      arg_datai[index] = int(value) 
+    for index, value in enumerate(self.dataf):
+      arg_dataf[index] = float(value)
+    for index, value in enumerate(self.coord):
+      arg_coord[index] = float(value)
+    for index, value in enumerate(self.coordi):
+      arg_coordi[index] = int(value)
+    val = libc.vec_map_loop(arg_dataf,arg_datai,arg_coord,arg_coordi)
+    return self
+
+seq = Sequence()
+seq.add_rot("rot1",0.245)
+#seq.add_rot("rot2",0.3*math.pi)
+seq.add_kick("kick1",1,3)
+seq.add_count("cnt",1,100)
+seq.add_expr("exp1","rot1.angle","0.245+cnt.count*0.00001")
+seq.track([1.0,1.0],[1,2])
+#seq.search("cnt.count")
+#rpn = RPNparser("3.2+2.1")
+#print seq.datai
+#print seq.dataf
+#print rpn.parser, rpn.constf
---
+++ anshu/rpncalc/track.c
@@ -0,0 +1,397 @@
+#include <math.h>
+#include <stdio.h>
+
+#define size 255
+#define INT 0
+#define FLOAT 1
+struct Elem{
+   int type;
+   union var{int i; double f; } data;
+};
+
+struct Elem_stack{
+    struct Elem st[size];
+    int elem_top;
+}el_stack;
+
+void init_elem_stack()
+{
+    el_stack.elem_top=-1;
+}
+
+void elem_pushInt(int item)
+{
+    el_stack.elem_top++;
+    el_stack.st[el_stack.elem_top].type = INT;
+    el_stack.st[el_stack.elem_top].data.i = (int)item;
+}
+
+void elem_pushFloat(float item)
+{
+    el_stack.elem_top++;
+    el_stack.st[el_stack.elem_top].type = FLOAT;
+    el_stack.st[el_stack.elem_top].data.f = (double)item;
+}
+
+struct Elem elem_pop()
+{
+    struct Elem item;
+    if(el_stack.elem_top<0)
+    {
+        item.type = 0;
+        item.data.i = 0;
+        return item;    
+    }
+    item.type = el_stack.st[el_stack.elem_top].type;
+    item.data = el_stack.st[el_stack.elem_top].data;
+    el_stack.elem_top--;
+    return(item);
+}
+
+
+void op_POW(){
+    struct Elem a,b;
+    b = elem_pop();
+    a = elem_pop();
+    if(a.type==INT && b.type==INT) 
+    {elem_pushInt(pow(a.data.i,b.data.i));}
+}
+
+void op_MUL(){
+    struct Elem a,b;
+    b = elem_pop();
+    a = elem_pop();
+    if(a.type==INT && b.type==INT) 
+    {elem_pushInt(a.data.i*b.data.i);}
+    if(a.type==FLOAT && b.type==INT) 
+    {elem_pushFloat(a.data.f*(double)b.data.i);}
+    if(a.type==INT && b.type==FLOAT) 
+    {elem_pushFloat((double)a.data.i*b.data.f);}
+    if(a.type==FLOAT && b.type==FLOAT) 
+    {elem_pushFloat(a.data.f*b.data.f);}
+}
+
+void op_DIV(){
+    struct Elem a,b;
+    b = elem_pop();
+    a = elem_pop();
+    if(a.type==INT && b.type==INT) 
+    {elem_pushInt((double)(a.data.i/b.data.i));}
+    if(a.type==FLOAT && b.type==INT) 
+    {elem_pushFloat((double)(a.data.f/(double)b.data.i));}
+    if(a.type==INT && b.type==FLOAT) 
+    {elem_pushFloat((double)((double)a.data.i/b.data.f));}
+    if(a.type==FLOAT && b.type==FLOAT) 
+    {elem_pushFloat((double)(a.data.f/b.data.f));}
+}
+
+void op_ADD(){
+    struct Elem a,b;
+    b = elem_pop();
+    a = elem_pop();
+    if(a.type==INT && b.type==INT) 
+    {elem_pushInt(a.data.i+b.data.i);}
+    if(a.type==FLOAT && b.type==INT) 
+    {elem_pushFloat(a.data.f+(double)b.data.i);}
+    if(a.type==INT && b.type==FLOAT) 
+    {elem_pushFloat((double)a.data.i+b.data.f);}
+    if(a.type==FLOAT && b.type==FLOAT) 
+    {elem_pushFloat(a.data.f+b.data.f);}
+}
+
+void op_SUB(){
+    struct Elem a,b;
+    b = elem_pop();
+    a = elem_pop();
+    if(a.type==INT && b.type==INT) 
+    {elem_pushInt(a.data.i-b.data.i);}
+    if(a.type==FLOAT && b.type==INT) 
+    {elem_pushFloat(a.data.f-(double)b.data.i);}
+    if(a.type==INT && b.type==FLOAT) 
+    {elem_pushFloat((double)a.data.i-b.data.f);}
+    if(a.type==FLOAT && b.type==FLOAT) 
+    {elem_pushFloat(a.data.f-b.data.f);}
+}
+
+void op_CONSTF(double constf[]){
+    struct Elem item;    
+    int iii;
+    item=elem_pop();
+    iii=item.data.i;
+    elem_pushFloat(constf[iii]);
+}
+
+void op_NUMi(int num){
+    elem_pushInt(num);
+}
+
+void op_NUMf(double num){
+    elem_pushFloat(num);
+}
+
+int op_add=1, op_sub=2, op_equ=3, op_mul=4, op_div=5, op_pow=6, op_constf=7, op_left=8, op_right=9;
+
+int decode(double dataf[], int datai[], int i)
+{
+    if(i%3==1)
+        return (i-1)/3;
+    else
+    {
+        if(i%3==2)
+            return (i-2)/3;
+        else
+            return i/3;
+    }
+}
+
+int is_operator(double dataf[],int datai[],int d)
+{
+    if((d%3==0)&&(decode(dataf,datai,d)!=op_constf))
+        return 1;
+    else 
+        return 0;  
+}
+
+int is_variable(int d)
+{
+    if(d%3==2)
+        return 1;
+    else 
+        return 0;
+}
+
+void call_ops(int index)
+{
+    switch(index)
+    {
+        case 1: op_ADD(); break;
+        case 2: op_SUB(); break;
+        case 4: op_MUL(); break;
+        case 5: op_DIV(); break;
+        case 6: op_POW(); break;
+    }
+}
+
+#define make_vecmap(NAME,ND) \
+  int  map_vec_##NAME(double dataf[], int datai[], int elem_id, double coord[], int np) { \
+    int iii,exit_code; exit_code=0;         \
+    for(iii=0;iii<np;iii=iii+1){   \
+      exit_code=map_##NAME(dataf,datai,elem_id,&coord[iii*ND]);  \
+      if (exit_code<0){ break;};	\
+    }; \
+         return exit_code;       \
+  }; \
+
+#define rot2d_TYPE 1
+
+int map_rot2d(double dataf[], int datai[], int elem_id, double coord[]) {
+  double cx,sx,x,y;
+  int elem_floatid;
+  elem_floatid=datai[elem_id+2];
+  cx=dataf[elem_floatid+1]; 
+  sx=dataf[elem_floatid+2];
+  x=coord[0]; y=coord[1];
+  coord[0]= cx*x+sx*y;
+  coord[1]=-sx*x+cx*y;
+  return 1;
+};
+
+int update_rot2d(double dataf[], int datai[], int elem_floatid) {
+  double angle;
+  angle=datai[elem_floatid];
+  dataf[elem_floatid+1]=cos(angle);
+  dataf[elem_floatid+2]=sin(angle);
+  return 1;
+};
+
+int update_kick2d(double dataf[], int datai[]) {
+  return 1;
+};
+
+int update_counter(double dataf[], int datai[]) {
+  return 1;
+};
+
+void update_gen(double dataf[], int datai[], int elem_id,int resi, double resf)
+{
+    int res_into,temp,type_id;
+    res_into = datai[elem_id+4];
+    temp = (res_into-2)/3;
+    if(temp%2==0)
+        datai[temp/2]=resi;
+    else
+        dataf[(temp-1)/2]=resf;
+    type_id = datai[elem_id+1];
+    switch(type_id){
+        case 1: update_rot2d(dataf,datai,(temp-1)/2); break;
+        case 2: update_kick2d(dataf,datai); break;
+        case 3: update_counter(dataf,datai); break;
+    }
+    
+}
+
+make_vecmap(rot2d,2)
+
+#define kick2d_TYPE 2
+
+int map_kick2d(double dataf[], int datai[], int elem_id, double coord[]) {
+  double kv;
+  int ord, elem_floatid;
+  elem_floatid=datai[elem_id+2];
+  kv=dataf[elem_floatid]; 
+  ord=datai[elem_id+4];
+  coord[1]+=kv*pow(coord[0],ord);
+  return 1;
+};
+
+make_vecmap(kick2d,2);
+
+#define counter_TYPE 3
+
+int map_counter(double dataf[], int datai[], int elem_id, double coord[]) {
+  int count,limit;
+  limit=datai[elem_id+5];
+  count=datai[elem_id+4]++;
+  if (count<limit){
+    return 1;
+  } else {
+    return -1;
+  };
+};
+
+make_vecmap(counter,2);
+
+#define expr_TYPE 4
+
+int map_expr(double dataf[], int datai[], int elem_id, double coord[])
+{
+    int i,len,temp;
+    struct Elem out;
+    int *results;
+    double *constf;
+    results = &datai[elem_id+6];
+    constf = &dataf[datai[elem_id+2]];
+    len = datai[elem_id+5];
+    init_elem_stack();
+    for(i=0;i<len;i++)
+    {
+        if(is_operator(dataf,datai,results[i])||(decode(dataf,datai,results[i])==op_constf))
+        {   
+            if(decode(dataf,datai,results[i])==op_constf)
+            {
+                op_CONSTF(constf);
+                continue;
+            }
+            call_ops(decode(dataf,datai,results[i]));
+        }
+        else
+        {
+            if(is_variable(results[i]))
+            {
+                temp = decode(dataf,datai,results[i]);
+                if(temp%2==0)
+                    op_NUMi(datai[temp/2]);
+                else
+                    op_NUMf(dataf[(temp-1)/2]);
+                continue;
+            }
+            op_NUMi(decode(dataf,datai,results[i]));
+        }
+    }
+    out = elem_pop();
+    if(out.type==INT) 
+    {
+        update_gen(dataf,datai,elem_id,out.data.i,0);
+        printf("%d\n",datai[(decode(dataf,datai,datai[elem_id+4]))/2]);  
+    }  
+    if(out.type==FLOAT)
+    {
+        update_gen(dataf,datai,elem_id,0,out.data.f);
+        printf("%f\n",dataf[(decode(dataf,datai,datai[elem_id+4])-1)/2]);
+    }
+    return 1;
+}
+
+make_vecmap(expr,2);
+
+/*
+#define block_TYPE 5
+
+int vec_map_block(double dataf[], int datai[], int elem_id, double coord[], int coordi[]){
+  int *elem_list,elem_type,retval,elem_next;
+  elem_list=&datai[elem_id+3];
+  while (*(elem_list++)!=-1){
+    elem_id=*elem_list;
+       elem_type=datai[elem_id+1]; 
+       switch (elem_type) {
+      case rot2d_TYPE:
+        retval=map_vec_rot2d(dataf, datai, elem_id, coord, coordi);
+        break;
+      case kick2d_TYPE:
+        retval=map_vec_kick2d(dataf, datai, elem_id, coord, coordi);
+        break;
+      case counter_TYPE:
+        retval=map_vec_counter(dataf, datai, elem_id, coord, coordi);
+        break;
+       };
+       if (retval<1) {return retval;}
+    };
+    elem_next=datai[elem_id+3];
+    return elem_next;
+};
+
+make_vecmap(block,2);
+
+void vec_map_loop(double dataf[], int datai[], int elem_id, double coord[], int coordi[]) {
+  int elem_next;  
+  elem_next=datai[elem_id+3];
+  while(elem_next>=0){
+    elem_next= vec_map_block(dataf, datai, elem_next, coord, coordi);
+  };
+};*/
+
+/* datai={elem_id,elem_type,elem_floatid,next_elem,other...}
+ */
+
+int vec_map_loop(double dataf[], int datai[], double coord[], int coordi[]) {
+  int retval,elem_id,elem_type,elem_next;
+  elem_id=0;
+  retval=1;
+  while(retval>0){
+    elem_id=datai[elem_id];
+    elem_type=datai[elem_id+1];
+    //elem_floatid=datai[elem_id+2];
+    elem_next=datai[elem_id+3];	
+    switch (elem_type) {
+      case rot2d_TYPE:
+        retval=map_vec_rot2d( dataf, datai, elem_id, coord, 1);
+        break;
+      case kick2d_TYPE:
+        retval=map_vec_kick2d( dataf, datai, elem_id, coord, 1);
+        break;
+      case counter_TYPE:
+        retval=map_vec_counter( dataf, datai, elem_id, coord, 1);
+        break;
+      case expr_TYPE:
+        retval=map_vec_expr( dataf, datai, elem_id, coord, 1);
+        break;
+    };
+    elem_id=elem_next;
+  };
+  return 1;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
