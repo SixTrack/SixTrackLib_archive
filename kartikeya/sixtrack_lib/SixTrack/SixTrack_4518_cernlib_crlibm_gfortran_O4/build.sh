d91f17695257606ddd42373afe6c67d2f4dc2b30
kartikeya singh <kartikeya107@gmail.com>
2014-07-03 04:10:31 +0530

    updated build.sh

--- kartikeya/sixtrack_lib/SixTrack/SixTrack_4518_cernlib_crlibm_gfortran_O4/build.sh
+++ kartikeya/sixtrack_lib/SixTrack/SixTrack_4518_cernlib_crlibm_gfortran_O4/build.sh
@@ -1,6 +1,6 @@
-gcc -c trackAPI.c
-gfortran -c  track.f
-gfortran  -m32 -o SixTrack_4518_cernlib_crlibm_gfortran_O4 track.o sixve.o sixvefox.o dabnews.o lielib.o trackAPI.o -lgraflib -lgrafX11 -lpacklib crlibm/crlibm.a
+gcc -c SixTrackLib.c
+gfortran -c  track1.f
+gfortran  -m32 -o SixTrack_4518_cernlib_crlibm_gfortran_O4 track1.o SixTrackLib.o sixve.o sixvefox.o dabnews.o lielib.o  -lgraflib -lgrafX11 -lpacklib crlibm/crlibm.a
 ./SixTrack_4518_cernlib_crlibm_gfortran_O4
 python read_fortbin.py
 
