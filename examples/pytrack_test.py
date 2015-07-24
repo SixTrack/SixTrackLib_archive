9fa202b0b01b5a89d09cb1be747c0a7d9e55af03
Aviral <aviral.2815@gmail.com>
2015-07-24 20:05:51 +0800

    maps changed

--- examples/pytrack_test.py
+++ examples/pytrack_test.py
@@ -26,26 +26,22 @@ d=SixDump.open('med_80_mo_3_s1_4-6_30/dump.dat.gz')
 p=Bunch(**d.get_particles(0,0))
 res={}
 res[0]=p
-# print p
 for iii,(name,ccc,el) in enumerate(out):
-    print el.name
-    # if hasattr(el,'rel'):
-    #     print el.rel
-    # el.track(p)
-    # res[iii+1]=(name,el,p.copy())
+    el.track(p)
+    res[iii+1]=(name,el,p.copy())
 
-# for idx in range(0,d.elem.shape[1]):
-#   iii=d.elem[0,idx,0]
-#   name,el,p=res[iii]
-#   print d.name[0,idx,0],ccc,name
-#   print el
-#   berr=0
-#   for nn in 's x px y py tau delta'.split():
-#      ss=getattr(d,nn)[0,idx,0]
-#      pp=getattr(p,nn)[0]
-#      err=sqrt(sum((ss-pp)**2))/ss
-#      print "%-6s %23.16e %23.16e %23.16e"%(nn,err,ss,pp)
-#      if nn not in ['s','tau']:
-#        berr+=err
+for idx in range(0,d.elem.shape[1]):
+  iii=d.elem[0,idx,0]
+  name,el,p=res[iii]
+  print d.name[0,idx,0],ccc,name
+  print el
+  berr=0
+  for nn in 's x px y py tau delta'.split():
+     ss=getattr(d,nn)[0,idx,0]
+     pp=getattr(p,nn)[0]
+     err=sqrt(sum((ss-pp)**2))/ss
+     print "%-6s %23.16e %23.16e %23.16e"%(nn,err,ss,pp)
+     if nn not in ['s','tau']:
+       berr+=err
 
 
